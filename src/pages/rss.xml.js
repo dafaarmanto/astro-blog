a65da2b92df6a283d1bbaf49eafa8ec9771f7896
Dafa Armanto <randikadafa07@gmail.com>
2022-10-27 21:33:27 +0700

    Updating site in RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export const get = () =>
   rss({
     title: "Dafa Armanto | Blog",
     description: "My journey learning Astro",
-    site: "https://example.com",
+    site: "https://astro-blog-omega.vercel.app/",
     items: import.meta.glob("./**/*.md"),
     customData: "<language>en-us</language>",
   });
